f3fb1cd5af34059cd674f140e459854c253deec0
SatishC16 <satishchandan786@gmail.com>
2020-07-11 20:36:15 +0530

    Updates

---
+++ precache-manifest.9d8c88ceded4cd9d67dcf112ef3513ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3321c2d9446691af0a0a9fa9a18f73f",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "e8c1ddceef1f947c20db",
+    "url": "/react-tictactoe/static/css/main.ed8c42f2.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/react-tictactoe/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tictactoe/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c1ddceef1f947c20db",
+    "url": "/react-tictactoe/static/js/main.20c4a4e6.chunk.js"
+  },
+  {
+    "revision": "3fa13acf8cc8b96bed84",
+    "url": "/react-tictactoe/static/js/runtime-main.4821dfcf.js"
+  }
+]);
